678ea6c77ddf25ff9588d2fb9fc632ab25e6ebe3
Rengkat <successinmotion11@gmail.com>
2023-12-28 00:52:03 +0100

    using static middleware

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const path = require("node:path");
 const app = express();
-app.get("/", (req, res) => {});
+//middleware
+app.use(express.static("./public"));
+// the html can be added to the public folder and use the middleware
+// app.get("/", (req, res) => {
+//use send file which is base on the extension of the file
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// });
 app.all("*", (req, res) => {
   res.status(404).send("Sorry, 404 error");
 });
