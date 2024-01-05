16728a219a4e50714f87b5e25734130f144c7500
Rengkat <successinmotion11@gmail.com>
2024-01-05 05:46:33 +0100

    mangodb set up

--- task-manager/index.js
+++ task-manager/index.js
@@ -1,9 +1,19 @@
+const connectDB = require("./db/connect");
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const tasks = require("./router/task");
 const port = process.env.PORT || 3000;
 app.use(express.static("./public"));
 app.use(express.json());
 //routes
 app.use("/api/v1/tasks", tasks);
-app.listen(port, () => console.log("Server listening on port 3000..."));
+const start = async () => {
+  try {
+    await connectDB(process.env.MANGO_URI);
+    app.listen(port, () => console.log("Server listening on port 3000..."));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
