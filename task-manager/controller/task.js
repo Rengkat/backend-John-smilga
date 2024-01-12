b2ac5fda1b3a6cc472d718b9509931b76a08d233
Rengkat <successinmotion11@gmail.com>
2024-01-12 01:46:03 +0100

    error handling

--- task-manager/controller/task.js
+++ task-manager/controller/task.js
@@ -1,14 +1,15 @@
 const Task = require("../model/task");
 const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../error/custom-error");
 const getTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
   res.status(200).json({ tasks });
 });
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const singleTask = await Task.findOne({ _id: taskID });
   if (!singleTask) {
-    return res.status(4040).json({ msg: `no task with id ${taskID}` });
+    return next(createCustomError(`no task with id ${taskID}`, 404));
   } else {
     res.status(200).json({ task: singleTask });
   }
@@ -24,7 +25,7 @@ const editTask = asyncWrapper(async (req, res) => {
     runValidators: true,
   });
   if (!task) {
-    res.status(4040).json({ msg: "No such task" });
+    return next(createCustomError(`no task with id ${taskID}`, 404));
   } else {
     res.status(200).json({ tasks });
   }
@@ -34,7 +35,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const task = await Task.findByIdAndDelete({ task: taskID });
   const tasks = await Task.find({});
   if (!task) {
-    res.status(4040).json({ msg: "No such task" });
+    return next(createCustomError(`no task with id ${taskID}`, 404));
   } else {
     res.status(200).json({ tasks });
   }
--- task-manager/middleware/errorHandler.js
+++ task-manager/middleware/errorHandler.js
@@ -1,4 +1,9 @@
+const { CustomError } = require("../error/custom-error");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  return res.status(500).json({ success: false, msg: err });
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).json({ success: false, msg: err.message });
+  } else {
+    return res.status(500).json({ msg: "Something went wrong" });
+  }
 };
 module.exports = errorHandlerMiddleware;
