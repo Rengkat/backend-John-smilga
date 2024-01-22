be407e8f7ee32c884b9fb3e76d4eb3e17ce3e377
Rengkat <successinmotion11@gmail.com>
2024-01-22 02:05:39 +0100

    refactor model

--- storeAPI/controller/products.js
+++ storeAPI/controller/products.js
@@ -1,7 +1,7 @@
 const asyncWrapper = require("../middleware/async");
 const Products = require("../model/products");
-const getProducts = asyncWrapper(async (req, res) => {
+const getProducts = async (req, res) => {
   const products = await Products.find({});
   res.status(200).json({ success: true, data: products });
-});
+};
 module.exports = { getProducts };
--- storeAPI/model/products.js
+++ storeAPI/model/products.js
@@ -1,3 +1,32 @@
 const mongoose = require("mongoose");
-const ProductSchema = new mongoose.Schema({});
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name must be provided"],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  price: {
+    type: Number,
+    required: [true, "Price must be provided"],
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ["ikea", "liddy", "caressa", "marcos"],
+      message: "{VALUE} is not supported",
+    },
+    // enum: ["ikea", "liddy", "caressa", "marcos"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 module.exports = mongoose.model("Products", ProductSchema);
