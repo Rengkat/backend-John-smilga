90ccf654bb20ca7b2aad6aa6c5a33bbefed818b8
Rengkat <successinmotion11@gmail.com>
2024-01-29 02:19:40 +0100

    advance query with regex

--- storeAPI/controller/products.js
+++ storeAPI/controller/products.js
@@ -1,13 +1,16 @@
 const asyncWrapper = require("../middleware/async");
 const Products = require("../model/products");
 const getProducts = async (req, res) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObj = {}; // creating new obj with the query if true
   if (featured) {
     queryObj.featured = featured === "true" ? true : false;
   }
   if (company) {
     queryObj.company = company;
+  }
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" }; //using regex to fine product by name that contains the letter from the query
   }
   const products = await Products.find(queryObj);
   res.status(200).json({ success: true, data: products });
