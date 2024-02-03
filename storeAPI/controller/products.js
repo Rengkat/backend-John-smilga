fedc8c1e26ba315d5ce9a028abcbec31fe0b3439
Rengkat <successinmotion11@gmail.com>
2024-02-03 03:51:45 +0100

    sorting

--- storeAPI/controller/products.js
+++ storeAPI/controller/products.js
@@ -1,7 +1,7 @@
 const asyncWrapper = require("../middleware/async");
 const Products = require("../model/products");
 const getProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObj = {}; // creating new obj with the query if true
   if (featured) {
     queryObj.featured = featured === "true" ? true : false;
@@ -12,7 +12,15 @@ const getProducts = async (req, res) => {
   if (name) {
     queryObj.name = { $regex: name, $options: "i" }; //using regex to fine product by name that contains the letter from the query
   }
-  const products = await Products.find(queryObj).sort("-name price");
+  let result = Products.find(queryObj);
+  if (sort) {
+    //a time the sort string might be more than one so use
+    const sortList = sort.split(",").join("");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  const products = await result; // this is to avoid await before sorting
   res.status(200).json({ success: true, data: products });
 };
 module.exports = { getProducts };
