8e3f9e6491ef11ad866d65d8ac4f0e6897f93dff
Rengkat <successinmotion11@gmail.com>
2024-02-05 00:55:40 +0100

    pagination

--- storeAPI/controller/products.js
+++ storeAPI/controller/products.js
@@ -1,7 +1,7 @@
 const asyncWrapper = require("../middleware/async");
 const Products = require("../model/products");
 const getProducts = async (req, res) => {
-  const { featured, company, name, sort, field, limit } = req.query;
+  const { featured, company, name, sort, field } = req.query;
   const queryObj = {}; // creating new obj with the query if true
   if (featured) {
     queryObj.featured = featured === "true" ? true : false;
@@ -26,9 +26,10 @@ const getProducts = async (req, res) => {
     result = result.sort(fieldList);
   } else {
   }
-  if (limit) {
-    result = result.limit(limit); // the limit of data
-  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
   const products = await result; // this is to avoid await before sorting
   res.status(200).json({ success: true, data: products });
 };
