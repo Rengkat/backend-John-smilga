d5237427823bc2e66ac451c2afbdb20c162bfad5
Rengkat <successinmotion11@gmail.com>
2024-02-10 01:27:33 +0100

    JWT auth setup

--- storeAPI/controller/products.js
+++ storeAPI/controller/products.js
@@ -26,6 +26,7 @@ const getProducts = async (req, res) => {
     result = result.sort(fieldList);
   } else {
   }
+  //pagination
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
